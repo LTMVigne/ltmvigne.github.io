de11b755bbed38f4771a1bc520d2fb16a91653ce
cmsigrist <capucine.berger@epfl.ch>
2022-05-31 19:31:48 +0200

    Add images

--- src/pages/dynamique/Today.tsx
+++ src/pages/dynamique/Today.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import languedoc from '../../assets/languedoc.png';
 
 const Today: FC = () => {
   return (
@@ -8,6 +9,9 @@ const Today: FC = () => {
         <p className="indent-5 text-lg text-justify py-4">
           {`Il s’agit de la dernière vigne intra muros de la ville de Lausanne, qui on le rappelle, est le plus grand propriétaire foncier public viticole de Suisse. Cette vigne est utilisée à des fins pédagogiques pour les écoles environnantes. Deux employés communaux s’en occupent tout au long de l’année et les récoltes sont ensuite redistribuées aux élèves. La vigne s'étend sur 1000 m2 avec quelques 600 pieds de vignes produisant 800 kg de raisin par année de 14 variétés différentes. Les vignes furent arrachée en 1930, mais replantée en 1987à titre didactique et symbolique par la ville qui a acquis le terrain en 1982`}
         </p>
+        <div className="flex justify-center ">
+          <img id="milan" src={languedoc} alt="" className="w-[518px] h-[395px]" />
+        </div>
       </div>
     </div>
   );
--- src/pages/dynamique/Urbanisation.tsx
+++ src/pages/dynamique/Urbanisation.tsx
@@ -1,12 +1,15 @@
 import React, { FC } from 'react';
 import phylloxera from '../../assets/phylloxera.jpg';
 import avenues from '../../assets/voieries.png';
+import georgette from '../../assets/georgette.jpg';
+import gare from '../../assets/quartierGareDepuisMontbenon.jpg';
+import rosemont from '../../assets/avenueDeRosemont.jpg';
 
 const Urbanisation: FC = () => {
   return (
     <div id="urbanisation" className="">
       <div className="flex-1 self-center pb-8">
-        <h1 className="text-3xl text-left text-gray-900">Urbanisation</h1>
+        <h1 className="text-3xl text-left text-gray-900">{`L'urbanisation`}</h1>
         <div className="lg:flex lg:flex-wrap justify-between">
           <p className="flex-1 indent-5 text-lg text-justify pt-4 lg:pr-10">
             {`Jusqu’à présent nos recherches étaient centrées sur l’urbanisation comme 
@@ -20,51 +23,59 @@ const Urbanisation: FC = () => {
           devient compliquée, les récoltes sont compromises et les traitements nécessaires pour 
           protéger et reconstituer ces vignes sont très coûteux. Si une partie des propriétaires 
           parvient à reconvertir leurs cultures de vins en prairies et champs, la majorité se 
-          voient contraint de vendre leurs parcelles.`}
+          voient contraint de vendre leurs parcelles. Or au même moment, Lausanne connaît une explosion 
+          démographique, entre 1884 et 1910 sa population se voit quasiment doublé (de 33 340 à 64 446 habitants). 
+          L’enquête de Schnetzler révèle, quant-à-elle, l’insalubrité de la ville. Il y a un besoin imminent de 
+          désengorger le centre de Lausanne. L’urbanisation au sud et à l’est de la gare commence alors 
+          réellement, et ce au dépens des vignes, qui représentent, par leur emplacement avec leur 
+          vue dégagée sur le Léman mais aussi leur ensoleillement, des parcelles idéales.  `}
           </p>
-          <div className="pt-4 pl-4 text-left lg:text-center lg:w-1/6">
+          <div className="pt-6 pl-4 text-left lg:text-center lg:w-1/6">
             <img id="phyllo" src={phylloxera} alt="" className="w-[179px] h-[308px]" />
             <label htmlFor="phyllo">Phylloxera vastatrix. BIU Santé Paris.</label>
           </div>
         </div>
 
-     
-          <p className="indent-5 text-lg text-justify py-4 lg:pr-10">
-            {`Or au même moment, Lausanne connaît une explosion démographique, entre 1884 et 1910 
-            sa population se voit quasiment doublé (de 33 340 à 64 446 habitants). L’enquête de 
-            Schnetzler révèle, quant-à-elle, l’insalubrité de la ville. Il y a un besoin imminent de 
-            désengorger le centre de Lausanne. L’urbanisation au sud et à l’est de la gare commence alors 
-            réellement, et ce au dépens des vignes, qui représentent, par leur emplacement avec leur 
-            vue dégagée sur le Léman mais aussi leur ensoleillement, des parcelles idéales. Avec un modèle 
+        <p className="indent-5 text-lg text-justify py-4">
+          {`Avec un modèle 
             quasi Haussmannien, de grandes percées rectilignes, telles l’Avenue Fréderic-César-de-la-Harpe, 
             sont réalisées pour relier la vieille ville à la gare, jusqu’à Ouchy.`}
-          </p>
-          <div className="flex justify-center py-6">
-            <img id="avenues" src={avenues} alt="" className="w-[726.75px] h-[684px]"/>
-            <label htmlFor="avenues"></label>
-          </div>
-
-
-        <div className="lg:flex lg:flex-wrap">
-          <p className="indent-5 text-lg text-justify py-4 lg:pr-10">
-            {`Ces avenues sont bordées par de nouvelles constructions qui, à l’image de Paris, utilisent 
-            l’îlot comme matrice de construction. Un exemple type serait le quartier Georgette, où le 
-            bâti devient une interface entre la ville et les cours intérieurs.`}
-          </p>
-          <div className="flex-1 py-4 text-left lg:text-center"></div>
+        </p>
+        <div className="flex justify-center py-6">
+          <img id="avenues" src={avenues} alt="" className="w-[726.75px] h-[684px]" />
+          <label htmlFor="avenues"></label>
         </div>
 
-        <div className="lg:flex lg:flex-wrap">
-          <p className="indent-5 text-lg text-justify py-4 lg:pr-10">
-            {`Finalement, la ville se détache de ce mouvement au début du 20ème siècle, pour adopter 
+        <div>
+          <p className="indent-5 text-lg text-justify pt-4 pb-10">
+            {`Ces avenues sont bordées par de nouvelles constructions qui, à l’image de Paris, utilisent 
+            l’îlot comme matrice de construction. Un exemple type serait le quartier Georgette, où le 
+            bâti devient une interface entre la ville et les cours intérieurs.
+            Finalement, la ville se détache de ce mouvement au début du 20ème siècle, pour adopter 
             un principe typologique en adéquation parfaite avec sa géographie. Les “plots” de Lausanne 
             émergent, notamment autour de l’Avenu Juste Olivier. Ces villas autonomes, tout comme l’îlot, 
             offrent un filtre entre la rue et les jardins. L’avantage de ce modèle ne se révèle réellement 
             que lorsque l’on observe l’espace créé par le négatif des bâtiments. En effet, les intersections 
             entre chaque plot dévoile une multitude de vues croisées sur le lac et les montagnes. Ainsi, 
-            malgré la disparition des vignes, la ville parvient à retrouver son caractère bucolique. `}
+            malgré la disparition des vignes, la ville parvient à retrouver son caractère bucolique.`}
           </p>
-          <div className="flex-1 py-4 text-left lg:text-center"></div>
+
+          <div className="flex items-end justify-between w-full">
+            <div className="w-[370px] h-[384px] text-center">
+              <img id="georgette" src={georgette} alt="" className="w-[370px] h-[384px] mb-2" />
+              <label htmlFor="georgette">Vue sur le quartier de Georgette</label>
+            </div>
+
+            <div className="w-[384px] h-[343px] text-center">
+              <img id="georgette" src={gare} alt="" className="w-[384px] h-[343px] mb-2" />
+              <label htmlFor="georgette">Vue sur le quartier de la Gare depuis Montbenon</label>
+            </div>
+
+            <div className="w-[384px] h-[290px] text-center">
+              <img id="georgette" src={rosemont} alt="" className="w-[384px] h-[290px] mb-2" />
+              <label htmlFor="georgette">{`Vue des villas de l'avenue de la Rasude`}</label>
+            </div>
+          </div>
         </div>
       </div>
     </div>
